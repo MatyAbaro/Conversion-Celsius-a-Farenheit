05f96c2b2369cc3f9789d6e79626083f175c5b32
Matias Abaro <49999740+MatyAbaro@users.noreply.github.com>
2020-03-18 17:40:23 -0300

    Update TP3ejercicio7.java

--- TP3ejercicio7.java
+++ TP3ejercicio7.java
@@ -1,28 +1,27 @@
-
-package tp3ejercicio7;
-
-import java.util.Scanner;
-
-public class TP3ejercicio7 {
+public class CelsiusOnFarenheit {
 
     public static void main(String[] args) {
-       pasarGrados();
-    } 
-       
-    static void pasarGrados(){
-       float grados, resultado;
-       Scanner entrada = new Scanner(System.in);
-    try{
-        System.out.println("Ingrese los grados Celsius");
-        grados=entrada.nextFloat();
-        resultado= 32 + (9*grados/5);
+              Scanner entrada = new Scanner (System.in);
+              float grados, resultado;
+              boolean repetir = true;
+        do{      
+           System.out.println("Ingrese los grados Celsius:");
+           try{
+               grados=entrada.nextFloat();
+               repetir = false;
+               resultado= 32 + (9*grados/5);
                System.out.println("La conversion de "+grados+ " grados "+ " a grados farenheit es: "+resultado);
-               System.out.println("---FIN DEL PROGRAMITA---");
+               System.out.println("---FIN DEL PROGRAMITA---");          
            }
-       catch (Exception e){System.out.println("ATENCIóN: Ingrese los grados con coma (,)");
-       pasarGrados();
-       }
-       }
+           catch (Exception e){
+               entrada.nextLine();
+            System.out.println("---ERROR, INGRESA DATOS VÁLIDOS!!---");
+               
+           }
+        } while (repetir);
+  
+        
     }
     
+}
 
