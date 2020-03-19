3eb0d16e355851c2a34e8b7a4e0a25633fd975b6
Matias Abaro <49999740+MatyAbaro@users.noreply.github.com>
2020-03-19 13:36:24 -0300

    Update TP3ejercicio7.java
    En un principio el programa solamente mostraba la conversión de grados Celsius a Farenheit. Luego le agregué manejo de excepciones (por si el usuario ingresaba caraceres o Strings en lugares de datos enteros. Y luego para finalizarlo ( en una 3ra version) y para mejorar la estética del programa, le incorporé ventanas emergentes con JOptionPane. Cualquier sugerencia sobre mejora será bienvenida.

--- TP3ejercicio7.java
+++ TP3ejercicio7.java
@@ -1,21 +1,31 @@
+package celsiusonfarenheit;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 public class CelsiusOnFarenheit {
 
     public static void main(String[] args) {
-              Scanner entrada = new Scanner (System.in);
+        Scanner entrada = new Scanner (System.in);
               float grados, resultado;
               boolean repetir = true;
         do{      
-           System.out.println("Ingrese los grados Celsius:");
+           //System.out.println("Ingrese los grados Celsius:");
+           
            try{
-               grados=entrada.nextFloat();
+               //grados=entrada.nextFloat();
+               grados = Integer.parseInt(JOptionPane.showInputDialog("Ingrese los datos Celsuis:"));
                repetir = false;
                resultado= 32 + (9*grados/5);//Cálculo matemático para la conversión.
-               System.out.println("La conversion de "+grados+ " grados "+ " a grados farenheit es: "+resultado);
-               System.out.println("---FIN DEL PROGRAMITA---");          
+               //System.out.println("La conversion de "+grados+ " grados "+ " a grados farenheit es: "+resultado);
+               //System.out.println("---FIN DEL PROGRAMITA---");          
+               JOptionPane.showMessageDialog(null,"La conversion de "+grados+ " grados "+ " a grados farenheit es: "+resultado );
+               JOptionPane.showMessageDialog(null,"FIN DEL PROGRAMITA");
            }
            catch (Exception e){  //Catch de posible error de introducir char, Strings o Double con punto en lugar de utilizar una coma (,).
-               entrada.nextLine();
-            System.out.println("---ERROR, INGRESA DATOS VÁLIDOS!!---");
+               //entrada.nextLine();
+            //System.out.println("---ERROR, INGRESA DATOS VÁLIDOS!!---");
+            JOptionPane.showMessageDialog(null,"---ERROR, INGRESA DATOS VÁLIDOS!!---");
                
            }
         } while (repetir);
@@ -23,5 +33,6 @@ public class CelsiusOnFarenheit {
         
     }
     
-}
+    }
+    
 
